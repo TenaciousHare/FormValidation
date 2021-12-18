5b6a0a701e01479f78bd90c323dd084c5a166735
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2021-12-18 18:58:55 +0100

    JS file - added validation for name input

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+const data = {};
 
+const nameInput = document.getElementById('name');
 
-const nameInput = document.querySelector('#name');
-nameInput.addEventListener('change', (e) => {
-    console.log(e.target.value);
+nameInput.addEventListener('change', () => {
+    const minLength = 2;
+    const hasSpecialCharsOrDigits = /[^AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż\s]/gim;
+
+    if (nameInput.value.length <= minLength) {
+        nameInput.parentNode.appendChild(createValidator("Your name should have at least three characters!"));
+    } else if (hasSpecialCharsOrDigits.test(nameInput.value)) {
+        nameInput.parentNode.appendChild(createValidator("Your name should not include any special characters or digits!"));
+    }
+    else {
+        data.name = nameInput.value;
+        removeValidator(nameInput)
+    }
 })
+
+
+function createValidator(txt) {
+    const validator = document.createElement('p');
+    validator.classList.add('validator');
+    validator.innerText = txt;
+    return validator;
+}
+
+function removeValidator(element) {
+    const validators = document.querySelectorAll('.validator');
+    const parent = element.parentNode;
+    validators.forEach(validator => parent.removeChild(validator));
+}
