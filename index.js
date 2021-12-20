fc89d47fd80b0496c237fdb9d02f335b0c233187
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2021-12-20 21:26:52 +0100

    JS file - added validation for password and confirmation inputs

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const data = {};
 
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
+const passInput = document.getElementById('password');
+const confInput = document.getElementById('confirm');
 
 nameInput.addEventListener('change', () => {
     const minLength = 2;
-    const hasSpecialCharsOrDigits = /[^AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż\s]/gim;
+    const specialCharOrDigitsReg = /[^AaĄąBbCcĆćDdEeĘęFfGgHhIiJjKkLlŁłMmNnŃńOoÓóPpRrSsŚśTtUuWwYyZzŹźŻż\s]/gim;
+    const hasSpecialCharsOrDigits = specialCharOrDigitsReg.test(nameInput.value);
 
     if (nameInput.value.length <= minLength) {
         nameInput.parentNode.appendChild(createValidator("Your name should have at least three characters!"));
-    } else if (hasSpecialCharsOrDigits.test(nameInput.value)) {
+    } else if (hasSpecialCharsOrDigits) {
         nameInput.parentNode.appendChild(createValidator("Your name should not include any special characters or digits!"));
     }
     else {
@@ -37,7 +40,51 @@ emailInput.addEventListener('change', () => {
 
 })
 
+passInput.addEventListener('change', () => {
+    const minLength = 8;
 
+    const oneBigLetterReg = /[A-Z]{1}/g;
+    const checkHowManyBigLetters = passInput.value.match(oneBigLetterReg);
+    const hasOneBigLetter = checkHowManyBigLetters.length === 1;
+
+    const atLeastOneDigitReg = /\d+/g;
+    const hasAtLeastOneDigit = atLeastOneDigitReg.test(passInput.value);
+    const atLeastOneSpecialCharReg = /[^A-Za-z0-9]+/g;
+    const hasAtLeastOneSpecialChar = atLeastOneSpecialCharReg.test(passInput.value);
+    console.log(`Password: ${passInput.value}`)
+    console.log(`Min length: ${passInput.value.length >= minLength}`);
+    console.log(`One Big Letter: ${hasOneBigLetter}`);
+    console.log(`Check How Much Big Letter: ${checkHowManyBigLetters}`);
+    console.log(`At least one digit: ${hasAtLeastOneDigit}`);
+    console.log(`At least one special char: ${hasAtLeastOneSpecialChar}`);
+    if (passInput.value.length <= minLength) {
+        passInput.parentNode.appendChild(createValidator("Your password should have at least eight characters!"));
+    }
+    else if (!hasOneBigLetter) {
+        passInput.parentNode.appendChild(createValidator("Your password should have one character in uppercase!"));
+
+    }
+    else if (!hasAtLeastOneDigit) {
+        passInput.parentNode.appendChild(createValidator("Your password should have at least one digit!"));
+    }
+    else if (!hasAtLeastOneSpecialChar) {
+        passInput.parentNode.appendChild(createValidator("Your password should have at least one special character!"));
+    }
+    else {
+        data.password = passInput.value;
+        removeValidator(passInput);
+    }
+})
+confInput.addEventListener('change', () => {
+    isTheSameAsPassword = confInput.value === data.password;
+    if (!isTheSameAsPassword) {
+        confInput.parentNode.appendChild(createValidator("Your password should be the same in both fields!"));
+    }
+    else {
+        data.confirmation = confInput.value;
+        removeValidator(confInput);
+    }
+})
 function createValidator(txt) {
     const validator = document.createElement('p');
     validator.classList.add('validator');
@@ -50,3 +97,4 @@ function removeValidator(element) {
     const parent = element.parentNode;
     validators.forEach(validator => parent.removeChild(validator));
 }
+
