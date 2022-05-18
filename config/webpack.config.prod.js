257c9734c421a42584ba766bd94d0ab7683bc106
Paweł Zajączkowski <pawel.zajaczkowski91@gmail.com>
2022-05-18 20:38:18 +0200

    Form cleanup added / webpack.config.prod repaired

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,75 +1,40 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+const baseConfig = require('./webpack.config');
 
-module.exports = {
-    mode: 'production',
-    entry: {
-        main: './src/index.js',
+const prodConfig = {...baseConfig};
+
+prodConfig.mode = 'production';
+prodConfig.output.publicPath = "/FormValidation/";
+delete prodConfig.devServer;
+prodConfig.module.rules[1] = {
+    test: /\.css$/,
+    use: [MiniCssExtractPlugin.loader, 'css-loader'],
+};
+prodConfig.module.rules[2] =  {
+    test: /\.(sass|scss)$/,
+    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+};
+prodConfig.module.rules[3] =  {
+    test: /\.(jpg|svg|png|gif|jpeg)$/,
+    type: 'asset/resource',
+    generator: {
+        filename: 'images/[name]-[contenthash:6][ext][query]'
     },
-    output: {
-        filename: 'js/index.js',
-        path: path.resolve(__dirname, '../', 'dist'),
-        publicPath: "/FormValidation/",
-    },
-    module: {
-        rules: [
-            {
-                test: /\.txt$/,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
-            {
-                test: /\.(sass|scss)$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
-            },
-            {
-                test: /\.(jpg|svg|png|gif|jpeg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name]-[contenthash:6][ext][query]'
-                },
-                use: [{
-                    loader: 'image-webpack-loader',
-                    options: {
-                        mozjpeg: {
-                            quality: 70,
-                            progressive: true
-                        }
-                    }
-                }],
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        ["@babel/preset-env", {
-                            useBuiltIns: 'usage',
-                            corejs: "2.0.0"
-                        }],
-                    ],
-                    plugins: ["@babel/plugin-proposal-class-properties"]
-                }
-            },
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: "Form Validation",
-            template: "index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name]-[contenthash:6].css',
-        }),
+    use: [{
+        loader: 'image-webpack-loader',
+        options: {
+            mozjpeg: {
+                quality: 70,
+                progressive: true
+            }
+        }
+    }],
+};
+
+prodConfig.plugins.push( new MiniCssExtractPlugin({
+    filename: 'css/[name]-[contenthash:6].css',
+}),);
 
 
-    ]
-}
+module.exports = prodConfig;
--- src/data/DataFromInputs.js
+++ src/data/DataFromInputs.js
@@ -12,6 +12,13 @@ class DataObject {
     setProp(propName, value){
         this[propName] = value;
     }
+    resetValues(){
+        this.name = '';
+        this.email = '';
+        this.password = '';
+        this.confirmation = '';
+        this.rodo = false;
+    }
 };
 
 export const DataFromInputs = new DataObject();
